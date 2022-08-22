c26e7541931ca47214f6e4c7cea3ee108269de7c
Ivan <adamovskij00@mail.ru>
2022-08-22 22:18:10 +0300

    add weather actions

--- src/actions/weatherActions.ts
+++ src/actions/weatherActions.ts
@@ -3,22 +3,28 @@ import {
   FETCH_WEATHER,
   FETCH_WEATHER_ERROR,
   FETCH_WEATHER_SUCCESS,
-} from '../constants/actions'
+} from 'constant/actions';
+import { CurrentWeatherDataType } from 'types';
 
-import { CurrentWeatherDataType } from './types'
+export const fetchWeather = () =>
+  ({
+    type: FETCH_WEATHER,
+  } as const);
 
-export const fetchWeather = () => ({
-  type: FETCH_WEATHER,
-})
-export const fetchWeatherSuccess = (dataWeather: CurrentWeatherDataType[]) => ({
-  type: FETCH_WEATHER_SUCCESS,
-  payload: { dataWeather },
-})
-export const fetchWeatherError = (errorMessage: string) => ({
-  type: FETCH_WEATHER_ERROR,
-  payload: { errorMessage },
-})
-export const changeWeatherApi = (nameApi: string) => ({
-  type: CHANGE_WEATHER_API,
-  payload: { nameApi },
-})
+export const fetchWeatherSuccess = (dataWeather: CurrentWeatherDataType[]) =>
+  ({
+    type: FETCH_WEATHER_SUCCESS,
+    payload: dataWeather,
+  } as const);
+
+export const fetchWeatherError = (errorMessage: string) =>
+  ({
+    type: FETCH_WEATHER_ERROR,
+    payload: errorMessage,
+  } as const);
+
+export const changeWeatherApi = (nameApi: string) =>
+  ({
+    type: CHANGE_WEATHER_API,
+    payload: nameApi,
+  } as const);
