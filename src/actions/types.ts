2276e1497004d684259132ce32cf0b7a64cbdc16
Ivan <adamovskij00@mail.ru>
2022-08-22 22:18:27 +0300

    add actions types

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,13 +1,28 @@
-export type CoordinatesType = {
-  lat: number;
-  lon: number;
-}
+import {
+  fetchLocation,
+  fetchLocationError,
+  fetchLocationSuccess,
+  updateCoordinates,
+  updateCountyCode,
+  updateLocationName,
+} from './locationActions';
+import {
+  changeWeatherApi,
+  fetchWeather,
+  fetchWeatherError,
+  fetchWeatherSuccess,
+} from './weatherActions';
 
-export type CurrentWeatherDataType = {
-  date: string;
-  temp: number;
-  weatherIcon: string;
-  weatherMain: string;
-  wind: number;
-  code: number;
-}
+export type LocationActionCreatorTypes =
+  | ReturnType<typeof fetchLocation>
+  | ReturnType<typeof fetchLocationSuccess>
+  | ReturnType<typeof fetchLocationError>
+  | ReturnType<typeof updateLocationName>
+  | ReturnType<typeof updateCountyCode>
+  | ReturnType<typeof updateCoordinates>;
+
+export type FetchActionCreatorTypes =
+  | ReturnType<typeof fetchWeather>
+  | ReturnType<typeof fetchWeatherSuccess>
+  | ReturnType<typeof fetchWeatherError>
+  | ReturnType<typeof changeWeatherApi>;
