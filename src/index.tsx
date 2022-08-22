7a1fc59617e640052dcd4d9ae9a4fd1db65cd9f0
Ivan <adamovskij00@mail.ru>
2022-08-22 23:03:50 +0300

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
-import { App } from './app/App'
+import App from './app';
+import { store } from './store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
-)
+const rootContainer = document.getElementById('root');
+if (!rootContainer) throw new Error('Failed to find the root element');
+const root = createRoot(rootContainer);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
--- src/theme.ts
+++ src/theme.ts
@@ -1,85 +1,19 @@
-// Fonts
-const font = 'Gill Sans, sans-serif'
-
-// Color palette
-const black = '#000000'
-const darkGray = '#00000029'
-const white = '#ffffff'
-const error = '#E25837'
-const pink = '#9A40D3'
-const yellow = '#F4E048'
-const green = '#37E29A'
-
-const boxShadows = [
-  'box-shadow: 4px 4px 8px #00000029',
-]
-
-const size = {
-  xs: 550,
-  small: 768,
-  med: 992,
-  large: 1200,
-  full: 100,
-}
+const orange = '#ff9900';
+const lightPink = '#cc9999';
+const black = '#000000';
+const lightBlack = '#222222';
+const grey = '#9b9b9b';
+const lightGrey = '#444433';
+const darkGrey = '#555555';
 
 export default {
-  size,
-  boxShadows,
-  font,
-  opacity: {
-    opacity0: 0,
-    opacity50: 0.5,
-    opacity100: 1,
-  },
-  height: {
-    full: 100,
-  },
-  radius: {
-    roundedSm: 0.5,  // 8px
-    roundedMd: 1.5,  // 24px
-    roundedFull: 9999,
-  },
-  paddings: {
-    p025: 0.125, // 2px
-    p5: 1.25, // 20px
-    p7: 1.75, // 30px
-    p10: 2.5, // 40px
-  },
-  margins: {
-    m2: 0.625, // 10px
-    m5: 1.25, // 20px
-    m7: 1.75, // 30px
-    m10: 2.5, // 40px
-  },
-  textStyle: {
-    sm: {
-      fontSize: 1, // 16px
-      lineHeight: 1.5,
-    },
-    base: {
-      fontSize: 1.25, // 20px
-      lineHeight: 1.75,
-    },
-    lg: {
-      fontSize: 1.875, // 30px
-      lineHeight: 2.25,
-    },
-    xl: {
-      fontSize: 2.25, // 36px
-      lineHeight: 2.5,
-    },
-    xl2: {
-      fontSize: 3, // 48px
-      lineHeight: 1,
-    },
-  },
   colors: {
+    orange,
+    lightPink,
     black,
-    darkGray,
-    white,
-    error,
-    green,
-    yellow,
-    pink,
+    lightBlack,
+    grey,
+    lightGrey,
+    darkGrey,
   },
-}
+};
