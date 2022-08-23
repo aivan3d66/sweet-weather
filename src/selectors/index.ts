a251db84566e9a6208766158e513e5c0fa190ac6
Ivan <adamovskij00@mail.ru>
2022-08-23 18:20:43 +0300

    add calendarStateSelector

--- src/selectors/index.ts
+++ src/selectors/index.ts
@@ -1,9 +1,14 @@
-import { LocationStateType, WeatherStateType } from 'reducers/types';
+import {
+  CalendarStateType,
+  LocationStateType,
+  WeatherStateType,
+} from 'reducers/types';
 import { CombinedState } from 'redux';
 
 interface GlobalState {
   weatherState: WeatherStateType;
   locationState: LocationStateType;
+  calendarState: CalendarStateType;
 }
 
 export const weatherDataSelector = (state: CombinedState<GlobalState>) =>
@@ -14,3 +19,6 @@ export const weatherStateSelector = (state: CombinedState<GlobalState>) =>
 
 export const locationStateSelector = (state: CombinedState<GlobalState>) =>
   state.locationState;
+
+export const calendarStateSelector = (state: CombinedState<GlobalState>) =>
+  state.calendarState;
