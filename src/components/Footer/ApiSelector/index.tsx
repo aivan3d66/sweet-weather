acb0193ca8b2b686b196e6ab2e6a98dd304935a2
Ivan <adamovskij00@mail.ru>
2022-08-23 11:54:23 +0300

    add ApiSelector component

---
+++ src/components/Footer/ApiSelector/index.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { useTypedSelector } from 'hooks';
+import { changeWeatherApi, fetchWeather } from 'actions';
+import { weatherStateSelector } from 'selectors';
+import { apiNames, text, defaultValues, classNames } from 'constant';
+
+import {
+  SelectorButton,
+  SelectorContainer,
+  SelectorList,
+  SelectorListItem,
+  SelectorText,
+} from './styles';
+
+const ApiSelector: React.FC = () => {
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { nameAPI } = useTypedSelector(weatherStateSelector);
+
+  const changeView = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const changeMode = (e: React.MouseEvent<HTMLButtonElement>) => {
+    changeView();
+    const value = e.currentTarget.textContent;
+    if (value) {
+      dispatch(changeWeatherApi(value));
+    }
+    dispatch(fetchWeather());
+  };
+
+  return (
+    <SelectorContainer onClick={changeView}>
+      <SelectorButton>
+        <SelectorText>{nameAPI}</SelectorText>
+        <SelectorText>{isOpen ? text.arrowTop : text.arrowBottom}</SelectorText>
+      </SelectorButton>
+      <SelectorList
+        className={
+          isOpen ? defaultValues.emptyString : classNames.hideSelectMode
+        }
+      >
+        <SelectorListItem onClick={changeMode}>
+          {apiNames.openWeather}
+        </SelectorListItem>
+        <SelectorListItem onClick={changeMode}>
+          {apiNames.weatherbit}
+        </SelectorListItem>
+      </SelectorList>
+    </SelectorContainer>
+  );
+};
+
+export default React.memo(ApiSelector);
---
+++ src/components/Footer/ApiSelector/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+import theme from 'theme';
+
+export const SelectorContainer = styled.div`
+  width: ${theme.width.sm}px;
+  margin: ${theme.margins.m1}px auto ${theme.margins.m4}px auto;
+`;
+
+export const SelectorButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: ${theme.width.full}%;
+  padding: ${theme.paddings.p1}px;
+  color: ${theme.colors.white};
+  border: none;
+  cursor: pointer;
+  &:disabled {
+    cursor: default;
+    opacity: 0.8;
+  }
+`;
+
+export const SelectorText = styled.span`
+  text-transform: uppercase;
+`;
+
+export const SelectorList = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  width: ${theme.width.sm}px;
+  list-style: none;
+
+  &.hide_select_mode {
+    display: none;
+  }
+`;
+
+export const SelectorListItem = styled.button`
+  display: inline-block;
+  text-transform: uppercase;
+  padding: ${theme.paddings.p1}px;
+  text-align: start;
+  border: none;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
