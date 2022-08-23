d4d1fab8c226e1194e3c731d64a02a37bec86dfb
Ivan <adamovskij00@mail.ru>
2022-08-23 18:21:11 +0300

    add upd rootSaga

--- src/saga/index.ts
+++ src/saga/index.ts
@@ -1,11 +1,17 @@
 import { all } from 'redux-saga/effects';
 
 import {
+  calendarWatcher,
   localeStorageWatcher,
   locationWatcher,
   weatherWatcher,
 } from './watchers';
 
 export default function* rootSaga() {
-  yield all([locationWatcher(), weatherWatcher(), localeStorageWatcher()]);
+  yield all([
+    locationWatcher(),
+    weatherWatcher(),
+    localeStorageWatcher(),
+    calendarWatcher(),
+  ]);
 }
