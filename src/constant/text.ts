a0a83f7d02aa14d88a4bb6461e2b36853e2ce9dd
Ivan <adamovskij00@mail.ru>
2022-08-23 11:53:07 +0300

    add constants

---
+++ src/constant/text.ts
@@ -0,0 +1,12 @@
+export const text = {
+  today: 'Today',
+  pm: 'PM',
+  am: 'AM',
+  cross: '✖',
+  plus: '✙',
+  arrowTop: '▲',
+  arrowBottom: '▼',
+  degree: '°',
+  mc: ' m/c',
+  px: 'px',
+};
