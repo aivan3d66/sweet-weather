96066cc7910a56920ae933879dc61734212ca481
Ivan <adamovskij00@mail.ru>
2022-08-23 11:54:44 +0300

    add CurrentDay component

---
+++ src/components/Footer/WeatherList/CurrentDay/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { useTypedSelector } from 'hooks';
+import { text } from 'constant';
+import { getUrlIcon } from 'utils';
+import { weatherStateSelector } from 'selectors';
+
+import {
+  CurrentDayContainer,
+  CurrentDayIcon,
+  CurrentDayTemp,
+  CurrentDayText,
+  CurrentDayTitle,
+  CurrentDayWrapper,
+} from './styles';
+
+const CurrentDay: React.FC = () => {
+  const { weatherData, nameAPI } = useTypedSelector(weatherStateSelector);
+
+  return (
+    <CurrentDayContainer>
+      {weatherData[0] && (
+        <CurrentDayWrapper>
+          <CurrentDayIcon
+            src={getUrlIcon(nameAPI, weatherData[0].weatherIcon)}
+            alt={weatherData[0].weatherMain}
+            title={weatherData[0].weatherMain}
+          />
+          <CurrentDayTitle>
+            <CurrentDayText>{text.today}</CurrentDayText>
+            <CurrentDayTemp>
+              {weatherData[0].temp}
+              {text.degree}
+            </CurrentDayTemp>
+          </CurrentDayTitle>
+        </CurrentDayWrapper>
+      )}
+    </CurrentDayContainer>
+  );
+};
+
+export default React.memo(CurrentDay);
---
+++ src/components/Footer/WeatherList/CurrentDay/styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import theme from '../../../../theme';
+
+export const CurrentDayContainer = styled.div`
+  margin: auto ${theme.margins.m4}px;
+`;
+
+export const CurrentDayWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+
+  @media (min-width: ${theme.viewPorts.mobile}px) {
+    width: ${theme.width.exm}px;
+    margin: 0 auto ${theme.margins.m2}px auto;
+  }
+  @media (min-width: ${theme.viewPorts.desktop}px) {
+    width: ${theme.width.base}px;
+  }
+`;
+
+export const CurrentDayTitle = styled.p`
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+`;
+
+export const CurrentDayText = styled.span`
+  font-size: ${theme.textStyle.lg.fontSize}px;
+`;
+
+export const CurrentDayTemp = styled.span`
+  font-size: ${theme.textStyle.xxl.fontSize}px;
+`;
+
+export const CurrentDayIcon = styled.img`
+  width: 80px;
+  border-radius: ${theme.radius.roundedFull}%;
+
+  @media (min-width: ${theme.viewPorts.desktop}px) {
+    width: ${theme.width.eexm}px;
+  }
+`;
