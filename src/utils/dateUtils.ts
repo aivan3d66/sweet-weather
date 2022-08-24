8c1706e6b015c48d9d469e66afa8b5cb10a34e4b
Ivan <adamovskij00@mail.ru>
2022-08-24 09:57:28 +0300

    add getCurrentDate util

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,2 +1,10 @@
 export const getDate = (timeStamp: number): string =>
   new Date(timeStamp * 1000).toDateString();
+
+export const getCurrentDate = (value: Date) => {
+  let currentDate: string
+  const day = String(value.getDate()).padStart(2, '0');
+  const month = String(value.getMonth() + 1).padStart(2, '0');
+  const year = value.getFullYear();
+  return currentDate = day + '/' + month + '/' + year
+}
