9646bbe8b750c597eb09d77652c882d428a495eb
Ivan <adamovskij00@mail.ru>
2022-08-24 14:16:02 +0300

    add upd openWeatherApi name

--- src/constant/baseUrl.ts
+++ src/constant/baseUrl.ts
@@ -1,6 +1,6 @@
 export const baseUrl = {
   locationIqApi: 'https://us1.locationiq.com',
-  openWeatherApi: 'http://api.openweathermap.org',
+  openWeatherApi: 'https://api.openweathermap.org',
   openWeather: 'https://openweathermap.org',
   weatherBitApi: 'https://api.weatherbit.io',
   weatherBit: 'https://www.weatherbit.io',
