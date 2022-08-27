5cc78c160bebd841a9b7edee8fc09ce1cb1bab49
Ivan <adamovskij00@mail.ru>
2022-08-27 11:52:58 +0300

    add calendar types

---
+++ src/components/UserCalendar/types.ts
@@ -0,0 +1,43 @@
+export interface IResult {
+  accessRole: string;
+  defaultReminders: Array<any>;
+  etag: string;
+  items: Array<ITodosList>;
+  kind: string;
+  summary: string;
+  timeZone: string;
+  updated: string;
+}
+
+export interface ITodosList {
+  created: string;
+  creator: {
+    email: string;
+    self: boolean;
+  };
+  end: {
+    date: string;
+  };
+  etag: string;
+  eventType: string;
+  htmlLink: string;
+  iCalUID: string;
+  id: string;
+  kind: string;
+  organizer: {
+    email: string;
+    self: boolean;
+  };
+  reminders: {
+    useDefault: boolean;
+    overrides: Array<any>;
+  };
+  sequence: number;
+  start: {
+    date: string;
+  };
+  status: string;
+  summary: string;
+  transparency: string;
+  updated: string;
+}
