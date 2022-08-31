24aad30fb5051a0478d38e228e0323a53d33f18c
Ivan <adamovskij00@mail.ru>
2022-08-31 15:56:28 +0300

    add upd defaultValues

--- src/constant/defaultValues.ts
+++ src/constant/defaultValues.ts
@@ -2,6 +2,8 @@ export const defaultValues = {
   placeholder: 'Please enter action',
   emptyArray: '[]',
   emptyString: '',
+  city: 'Minsk',
+  code: 'BY',
   coordinates: {
     lat: 53.90454,
     lon: 27.561524,
