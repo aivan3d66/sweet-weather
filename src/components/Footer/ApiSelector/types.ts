783075d706692b039d79e60ee46d0c56953d8462
Ivan <adamovskij00@mail.ru>
2022-08-31 16:21:19 +0300

    add apiNames with types

---
+++ src/components/Footer/ApiSelector/types.ts
@@ -0,0 +1,7 @@
+export interface IApiNames {
+  selector: ['OpenWeather', 'Weatherbit'];
+  openWeather: string;
+  openWeatherGeocode: string;
+  weatherbit: string;
+  locationiq: string;
+}
--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,4 +1,7 @@
-export const apiNames = {
+import { IApiNames } from '@/components/Footer/ApiSelector/types';
+
+export const apiNames: IApiNames = {
+  selector: ['OpenWeather', 'Weatherbit'],
   openWeather: 'OpenWeather',
   openWeatherGeocode: 'OpenWeatherGeocode',
   weatherbit: 'Weatherbit',
